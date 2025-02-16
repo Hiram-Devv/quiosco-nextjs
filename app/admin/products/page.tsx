1aeb11590fde3880d63cd46b7ae1eb5a415c8f90
HiramDev <hiramdev02@gmail.com>
2025-02-16 12:00:56 -0600

    feature: showing the all products

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,10 +1,28 @@
+import ProductTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
+import { prisma } from "@/src/lib/prisma";
 
-export default function ProductsPage() {
+async function getProducts() {
+  const products = await prisma.product.findMany({
+    take: 10,
+    include: {
+      category: true,
+    },
+  });
 
-    return (
-      <>
-        <Heading>Administrar productos</Heading>
-      </>
-    )
-  }
+  return products;
+}
+
+export type ProductsWithCategory = Awaited<ReturnType<typeof getProducts>>;
+
+export default async function ProductsPage() {
+  const products = await getProducts();
+
+  return (
+    <>
+      <Heading>Administrar productos</Heading>
+
+      <ProductTable products={products} />
+    </>
+  );
+}
